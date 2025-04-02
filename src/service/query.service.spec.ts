d4a58c820c37cbe40469eb9daebac962a9fff6a2
Elie Soueid <eliegebransoued@gmail.com>
2025-04-02 09:35:21 +0200

    add tests for queryService

--- src/service/query.service.spec.ts
+++ src/service/query.service.spec.ts
@@ -1,18 +1,49 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { QueryService } from './query.service';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 describe('QueryService', () => {
-  let service: QueryService;
-
+  let queryService: QueryService;
+  let httpTesting: HttpTestingController;
+  const testUrl = '/api/data';
+  const mockResponse = { message: 'Success!' };
+  const body = { username: 'Pilex', password: '123' };
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [provideHttpClient(withFetch())],
+      providers: [
+        QueryService,
+        provideHttpClient(withFetch()),
+        provideHttpClientTesting(),
+      ],
     });
-    service = TestBed.inject(QueryService);
+
+    queryService = TestBed.inject(QueryService);
+    httpTesting = TestBed.inject(HttpTestingController);
+  });
+  afterEach(() => {
+    httpTesting.verify();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('it should perform a GET request', async () => {
+    const response$ = queryService.get(testUrl);
+    const responsePromise = firstValueFrom(response$);
+    const req = httpTesting.expectOne(testUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+    expect(await responsePromise).toEqual(mockResponse);
+  });
+
+  it('it should perfor a POST request', async () => {
+    const response$ = queryService.post(testUrl, body);
+    const responsePromise = firstValueFrom(response$);
+    const req = httpTesting.expectOne(testUrl);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockResponse);
+    expect(await responsePromise).toEqual(mockResponse);
   });
 });
