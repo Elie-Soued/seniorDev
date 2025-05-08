0415d573ee08b0eeeaedceb19a67dfcb23309c34
Elie Soueid <eliegebransoued@gmail.com>
2025-05-08 12:46:20 +0200

    removing environment.ts from git

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  URL: 'http://localhost:5000/tasks',
-  URL_LOGIN: 'http://localhost:5000/users/login',
-  URL_REGISTER: 'http://localhost:5000/users/register',
-};
