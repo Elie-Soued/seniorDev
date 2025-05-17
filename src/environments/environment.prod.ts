6b85ed37b04925dc9555a742a9c140181940a65a
Elie Soueid <eliegebransoued@gmail.com>
2025-05-17 14:23:47 +0200

    have different outputpath and env for dev, production and githubactions

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  URL: 'https://seniordevapi.pilexlaflex.com/tasks',
+  URL_LOGIN: 'https://seniordevapi.pilexlaflex.com/users/login',
+  URL_REGISTER: 'https://seniordevapi.pilexlaflex.com/users/register',
+};
