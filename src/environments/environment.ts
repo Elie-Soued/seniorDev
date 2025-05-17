39e1cb240486523e7cb8e615c99371609b495ff6
Elie Soueid <eliegebransoued@gmail.com>
2025-05-17 09:02:25 +0200

    adding the environment file back in git to test the build in github actions

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  URL: 'http://localhost:5000/tasks',
+  URL_LOGIN: 'http://localhost:5000/users/login',
+  URL_REGISTER: 'http://localhost:5000/users/register',
+};
